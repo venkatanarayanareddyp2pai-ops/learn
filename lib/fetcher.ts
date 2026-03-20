af650b4fccf00776f816ee38b76ee7f848875638
venkatanarayanareddyp2pai-ops <venkatanarayanareddy.p2pai@gmail.com>
2026-03-20 16:30:59 +0530

    Delete lib directory

--- lib/fetcher.ts
+++
@@ -1,6 +0,0 @@
-
-export async function fetcher<T = unknown>(url: string): Promise<T> {
-  const res = await fetch(url);
-  if (!res.ok) throw new Error(`fetch failed: ${res.status}`);
-  return res.json() as Promise<T>;
-}
